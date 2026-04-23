2a91ff5e697ac26391afb16db0c463458217b6ed
SavunOski <mincraftenerjisi@gmail.com>
2026-04-23 17:09:04 +0300

    find env in the correct place, safe from botter skiddos

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,4 +1,5 @@
-require("dotenv").config({ quiet: true });
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, ".env"), quiet: true });
 const { ethers } = require("ethers");
 const { initGemini } = require("./gemini");
 
